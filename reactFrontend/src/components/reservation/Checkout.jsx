d15860cedd1c01ada35d6f8f205e5b31eac7d585
abde chkir <abdechkir@example.com>
2024-11-26 00:41:44 +0100

    dropped commit

--- reactFrontend/src/components/reservation/Checkout.jsx
+++ reactFrontend/src/components/reservation/Checkout.jsx
@@ -615,11 +615,10 @@ const handleGoToContactPage = () => {
                     value={
                         `${isAddon1 ? 'GPS' : ''}` + 
                         (isAddon2 ? `, Conducteur supplémentaire` : '') + 
-                        (isAddon3 ? `, Siège bébé` : '')
+                        (isAddon3 ? `, Siège bébé` : '') || 'sans extensions'
                     } 
                     name="addons" 
                 />
-
                 <div>
                     <button type='submit' className='bg-red-600 text-white font-semibold rounded px-4 py-2'>Commander</button>
                 </div>
